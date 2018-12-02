9a8320ce3b977f7b716dab00ff3271b065df0486
denys <denyscontant@gmail.com>
2018-12-02 05:15:18 -0500

    Update day_2.py

--- day_2.py
+++ day_2.py
@@ -3,6 +3,7 @@ from collections import Counter
 with open('day_2.data') as f:
     data = f.read().split()
 
+# checksum, product of the number of id number with exactly 3 identical letter and number of id with exactly 2 identical letter
 threes = 0
 twos = 0
 for box in data:
@@ -13,8 +14,10 @@ for box in data:
 print('Part 1:',twos * threes)
 
 def close(id1, id2):
+    # true if id1 and id2 differ by one and only one character
     return sum(len(set(p)) for p in zip(id1,id2)) == (len(id1) + 1)
 
+# print common characters of id1 and id2
 for i in range(len(data)-1):
     for j in range(i+1,len(data)):
         if close(data[i],data[j]):
