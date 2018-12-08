8f9d14af9cc122fa3d0c8c3a34e4443397c55d30
denys <denyscontant@gmail.com>
2018-12-08 17:12:59 -0500

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -1,9 +1,17 @@
-from string import ascii_lowercase as units
+'''
+remove all lowercase-uppercase pairs, return the final length of the strings ( test data is length 16, actual data = 50000
+dabAcCaCBAcCcaDA  The first 'cC' is removed.
+dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+dabCBAcaDA        No further actions can be taken.
+'''
+
 
 with open('AOC2018_5.data', 'r') as f:
     data = f.read()
 
 def same_pol(i):
+    '''checks if two adjacent are a uppper-lower pair, tally the total number found, '''
     global n
     global reacts
     j = i+1
@@ -26,7 +34,14 @@ while i<len(data)-1:
 print('part 1:', len(data)-n)
 
 
+'''
+for all letters, remove all instances of uppercase-lowercase  (regardless of polarity), 
+fully react the remaining polymer, and measure its length.
+find the shortest length obtained
+'''
 # Part 2
+from string import ascii_lowercase as units
+
 temp = data[:]
 final_length = []
 for u in units:
